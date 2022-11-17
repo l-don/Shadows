784ba17b927b9f7926f2e2d129887f1d9a37403c
l-don <louis.donath@gmail.com>
2022-11-17 20:21:41 +0100

    movement + camera controll refactoring
    Using character controller component

--- ShadowsUnity/Assets/Scripts/PlayerMovement.cs
+++ ShadowsUnity/Assets/Scripts/PlayerMovement.cs
@@ -1,44 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody rb;
-    private Camera cam;
-    private const float Distance = 5f;
+    public CharacterController controller;
 
-    // Start is called before the first frame update
+    public float speed = 12f;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        cam = GetComponentInChildren<Camera>();
+        
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        Transform camT = cam.transform;
-        //when pressing w move character forward
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += camT.forward * (Distance * Time.deltaTime);
-
-        }
-        //when pressing s move character backwards 
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position -=camT.forward * (Distance * Time.deltaTime);
-        }
-        //when pressing a move character to the left
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position -= camT.right * (Distance * Time.deltaTime);
-        }
-        //when pressing d move character to the right
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += camT.right * (Distance * Time.deltaTime);
-        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        
+        //movement relative to the players direction of view
+        Vector3 move = transform.right * x + transform.forward * z;
+        
+        //use Char. controller to set movement
+        //time.deltaTime makes it framerate independent
+        controller.Move(move *speed * Time.deltaTime);
     }
 }
+//https://www.youtube.com/watch?v=_QajrabyTJc
