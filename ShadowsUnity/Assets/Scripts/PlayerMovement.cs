92cbf3400cdcbc921461832b05f66df4d4068077
l-don <louis.donath@gmail.com>
2022-11-19 15:53:18 +0100

    jumping + gravity added, jumping parkour

--- ShadowsUnity/Assets/Scripts/PlayerMovement.cs
+++ ShadowsUnity/Assets/Scripts/PlayerMovement.cs
@@ -8,23 +8,49 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
-    void Start()
-    {
-        
-    }
-    
+
+    //gravity
+    public float gravity = -9.81f;
+    private Vector3 velocity;
+
+    //groundcheck
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    public float jumpheight = 3f;
+
     void Update()
     {
+        //true if sphere collides with ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -10f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        
-        
+
+
         //movement relative to the players direction of view
         Vector3 move = transform.right * x + transform.forward * z;
-        
+
         //use Char. controller to set movement
         //time.deltaTime makes it framerate independent
-        controller.Move(move *speed * Time.deltaTime);
+        controller.Move(move * speed * Time.deltaTime);
+
+        //Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
 //https://www.youtube.com/watch?v=_QajrabyTJc
